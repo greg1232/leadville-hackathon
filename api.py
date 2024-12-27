45f5fed3f21da39afa1a97362151d3b9ff00c04e
Gregory Diamos <gregorydiamos@Air-Gregory.home>
2024-12-26 17:20:21 -0700

    first commit

---
+++ api.py
@@ -0,0 +1,12 @@
+
+def embed(text : str) -> List[float]:
+    pass
+
+
+def train(X : List[List[float]], y : List[int]) -> Model:
+    pass
+
+
+def classify(model : Model, text : str) -> int:
+    pass
+
